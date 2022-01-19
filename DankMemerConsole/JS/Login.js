0b4e83abc657d486fd1d9b37578b861efb5bdecd
Stevie White <stephenjeffreywhite@outlook.com>
2022-01-19 01:26:24 -0500

    Removed login script; adjusted styles; added more buttons

--- DankMemerConsole/JS/Login.js
+++
@@ -1,8 +0,0 @@
-﻿var buttons = document.querySelectorAll('button');
-if (buttons)
-{
-    for (i = 0; i < buttons.length; i++) {
-        if (buttons[i].innerText === 'Login')
-        { console.log('found the dumb button'); buttons[i].click(); }
-    }
-}
